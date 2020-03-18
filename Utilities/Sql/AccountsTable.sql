53e284af318de0dad244767024ab9ce497420baf
themorticianer <lucrurup@gmail.com>
2020-03-18 14:26:03 -0500

    pull

---
+++ Utilities/Sql/AccountsTable.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Accounts]
+(
+	[Username] VARCHAR(25) NOT NULL PRIMARY KEY, 
+    [Password] VARCHAR(25) NOT NULL, 
+    [Email] VARCHAR(50) NOT NULL, 
+    [Address] VARCHAR(50) NOT NULL
+)
