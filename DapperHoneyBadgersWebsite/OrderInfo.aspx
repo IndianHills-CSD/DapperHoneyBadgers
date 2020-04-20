d62fb9a43f02158e42404e783403c989c925f06c
Spencer <fergusonstf@yahoo.com>
2020-04-20 13:26:25 -0500

    Temp commit to allow upstream pull

--- DapperHoneyBadgersWebsite/OrderInfo.aspx
+++ DapperHoneyBadgersWebsite/OrderInfo.aspx
@@ -2,45 +2,10 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
-    <div class="center">     
-        <table class="infoTable">
-            <tr>
-                <th><strong>Order ID</strong></th>
-                <th colspan="2"><strong>Order Details</strong></th>
-            </tr>   <!-- END Headings Row -->
-<!-- 
-    All order id in table have placeholder link incase we wanted to give a brief order description here and a more detailed of an individual order on a separate page. This HTML table is a place holder. Maybe a better to use DataTable in load event to create a dynamic data table and StringBuilder class to create html code for the table rather than have a hard coded HTML table. 
--->
-            <tr>
-                <td><a href="#">Insert OrderID</a></td>
-                <td colspan="2">Insert OrderDate, ProductID, etc here</td>
-            </tr>   <!-- END Data Row -->
-            <tr>
-                <td><a href="#">Insert OrderID</a></td>
-                <td colspan="2">Insert OrderDate, ProductID, etc here</td>
-            </tr>   <!-- END Data Row -->
-            <tr>
-                <td><a href="#">Insert OrderID</a></td>
-                <td colspan="2">Insert OrderDate, ProductID, etc here</td>
-            </tr>   <!-- END Data Row -->
-            <tr>
-                <td><a href="#">Insert OrderID</a></td>
-                <td colspan="2">Insert OrderDate, ProductID, etc here</td>
-            </tr>   <!-- END Data Row -->
-            <tr>
-                <td><a href="#">Insert OrderID</a></td>
-                <td colspan="2">Insert OrderDate, ProductID, etc here</td>
-            </tr>   <!-- END Data Row -->
-            <tfoot>
-               <!-- colspan="" creates a single row 3 columns wide for the button to be in -->
-                <tr colspan="3">
-                    <td colspan="3">        
-                        <button class="homeBTN">
-                            Return Home
-                        </button>
-                    </td>
-                </tr>
-            </tfoot>
+    <div class="center">
+        <h1>Order Information</h1>
+        <table class="infoTable" id="orderTable" runat="server">
+            
         </table>
     </div>  <!-- END div center1 -->
 
--- DapperHoneyBadgersWebsite/OrderInfo.aspx.cs
+++ DapperHoneyBadgersWebsite/OrderInfo.aspx.cs
@@ -3,15 +3,206 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
+using System.Web.UI.HtmlControls;
 using System.Web.UI.WebControls;
 
 namespace DapperHoneyBadgersWebsite
 {
+    #region Temporary Classes for Testing, delete later
+    class TempOrder
+    {
+        string ID;
+        string DATE;
+        string ADDRESS;
+        string PRICE;
+
+        public TempOrder(int id)
+        {
+            ID = id.ToString();
+            DATE = id.ToString() + "/1/2001";
+            ADDRESS = id.ToString() + "99 9th Street";
+            PRICE = (id * 100).ToString();
+        }
+
+        // Does not necessarily need to be done in the order class itself, but having it here simplies the process.
+        public void AppendToRow(HtmlTableRow row )
+        {
+            // ID, to be clicked. Redirect to the OrderItems page with the id set.
+            HtmlTableCell cell = new HtmlTableCell();
+            Button btn = new Button();
+            btn.ID = ID;
+            btn.Text = ID;
+            btn.BackColor = System.Drawing.Color.Transparent;
+            btn.Click += new EventHandler( delegate ( object s, EventArgs e )
+            {
+                HttpContext.Current.Session.Add( "OrderID", (s as Button).ID );
+                HttpContext.Current.Response.Redirect( "OrderInfo.aspx" );
+            } );
+            cell.Controls.Add( btn );
+            row.Cells.Add( cell );
+
+            // Date, Address, and Price simply need to be displayed.
+            cell = new HtmlTableCell();
+            cell.InnerHtml = DATE;
+            row.Cells.Add( cell );
+
+            cell = new HtmlTableCell();
+            cell.InnerHtml = ADDRESS;
+            row.Cells.Add( cell );
+
+            cell = new HtmlTableCell();
+            cell.InnerHtml = PRICE;
+            row.Cells.Add( cell );
+        }
+    }
+
+    class TempItem
+    {
+        public string ID;
+        public string CategoryID;
+        public double PricePerUnit;
+        public int Qty;
+        public double TotalPrice { get { return PricePerUnit * Qty; } }
+
+        public TempItem( int ID )
+        {
+            this.ID = ID.ToString();
+            CategoryID = this.ID.ToString();
+            this.PricePerUnit = 1.01 * ID;
+            this.Qty = ID;
+        }
+
+        // Does not necessarily need to be done in the item class itself, but having it here simplies the process.
+        public void AppendToRow( HtmlTableRow row )
+        {
+            // ID, to be clicked. Redirect to the OrderItems page with the id set.
+            HtmlTableCell cell = new HtmlTableCell();
+            Button btn = new Button();
+            btn.ID = ID;
+            btn.Text = ID;
+            btn.BackColor = System.Drawing.Color.Transparent;
+            btn.Click += new EventHandler( delegate ( object s, EventArgs e )
+            {
+                HttpContext.Current.Session.Add( "ProductID", (s as Button).ID );
+                HttpContext.Current.Response.Redirect( "OrderInfo.aspx" );
+            } );
+            cell.Controls.Add( btn );
+            row.Cells.Add( cell );
+
+            // Date, Address, and Price simply need to be displayed.
+            cell = new HtmlTableCell();
+            cell.InnerHtml = DATE;
+            row.Cells.Add( cell );
+
+            cell = new HtmlTableCell();
+            cell.InnerHtml = ADDRESS;
+            row.Cells.Add( cell );
+
+            cell = new HtmlTableCell();
+            cell.InnerHtml = PRICE;
+            row.Cells.Add( cell );
+        }
+    }
+
+    #endregion
+
     public partial class OrderInfo : System.Web.UI.Page
     {
+        // Add our basic heading information.
+        private void PrepareOrderHeading(HtmlTableRow heading )
+        {
+            // ID
+            HtmlTableCell cell = new HtmlTableCell();
+            cell.Style.Add( "font-weight", "bold" );
+            cell.InnerHtml = "ID";
+            heading.Cells.Add( cell );
+
+            // Date
+            cell = new HtmlTableCell();
+            cell.Style.Add( "font-weight", "bold" );
+            cell.InnerHtml = "Date";
+            heading.Cells.Add( cell );
+
+            // Address
+            cell = new HtmlTableCell();
+            cell.Style.Add( "font-weight", "bold" );
+            cell.InnerHtml = "Address";
+            heading.Cells.Add( cell );
+
+            // Price
+            cell = new HtmlTableCell();
+            cell.Style.Add( "font-weight", "bold" );
+            cell.InnerHtml = "Price";
+            heading.Cells.Add( cell );
+        }
+        private void PrepareItemHeading( HtmlTableRow heading )
+        {
+            // ID
+            HtmlTableCell cell = new HtmlTableCell();
+            cell.Style.Add( "font-weight", "bold" );
+            cell.InnerHtml = "ID";
+            heading.Cells.Add( cell );
+
+            // Date
+            cell = new HtmlTableCell();
+            cell.Style.Add( "font-weight", "bold" );
+            cell.InnerHtml = "Date";
+            heading.Cells.Add( cell );
+
+            // Address
+            cell = new HtmlTableCell();
+            cell.Style.Add( "font-weight", "bold" );
+            cell.InnerHtml = "Address";
+            heading.Cells.Add( cell );
+
+            // Price
+            cell = new HtmlTableCell();
+            cell.Style.Add( "font-weight", "bold" );
+            cell.InnerHtml = "Price";
+            heading.Cells.Add( cell );
+        }
+
+        // Add in information related to an order.
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            // Clear any existing information.
+            orderTable.Controls.Clear();
 
+            // If we do not have a specific order specified, display all orders.
+            if ( HttpContext.Current.Session["OrderID"] == null )
+            {
+                // Prepare and add our heading row.
+                HtmlTableRow heading = new HtmlTableRow();
+                PrepareHeading( heading );
+                orderTable.Controls.Add( heading );
+
+                // Prepare our rows (hardcoded example for now.)
+                for ( int x = 0; x < 8; x++ )
+                {
+                    TempOrder order = new TempOrder( x );
+                    HtmlTableRow orderRow = new HtmlTableRow();
+                    order.AppendToRow( orderRow );
+                    orderTable.Controls.Add( orderRow );
+                }
+            }
+            // If we DO have a specific order specified, display its items instead.
+            else
+            {
+                // Prepare and add our heading row.
+                HtmlTableRow heading = new HtmlTableRow();
+                PrepareHeading( heading );
+                orderTable.Controls.Add( heading );
+
+                // Prepare our rows (hardcoded example for now.)
+                for ( int x = 0; x < 8; x++ )
+                {
+                    TempOrder order = new TempOrder( x );
+                    HtmlTableRow orderRow = new HtmlTableRow();
+                    order.AppendToRow( orderRow );
+                    orderTable.Controls.Add( orderRow );
+                }
+            }
         }
     }
 }
